57622899c126cbb209b81da06bb52da20ab6b786
IsraelMerlyn <merlyn142a@gmail.com>
2024-07-14 22:57:04 -0600

    first commit

---
+++ src/main/java/com/israelmerlyn/backend/usersapp/backend_users/BackendUsersApplication.java
@@ -0,0 +1,13 @@
+package com.israelmerlyn.backend.usersapp.backend_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/israelmerlyn/backend/usersapp/backend_users/BackendUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israelmerlyn.backend.usersapp.backend_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
